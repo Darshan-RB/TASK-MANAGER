355229f7569b0565b20c4ada2d50f79844eb411b
Darshan <darshan.rb2022cce@sece.ac.in>
2025-05-27 16:09:41 +0530

    Add root route

--- server/server.js
+++ server/server.js
@@ -175,3 +175,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
