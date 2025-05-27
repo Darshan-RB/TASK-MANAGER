ddae70c4e47702e8af48bf7b11038d4fa7a5e6fc
Darshan <darshan.rb2022cce@sece.ac.in>
2025-05-27 16:07:55 +0530

    Fix: use dynamic port from env

--- server/server.js
+++ server/server.js
@@ -170,7 +170,8 @@ app.get('/users', async (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
